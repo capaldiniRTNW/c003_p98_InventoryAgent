376ae50070ae8ec746d39e1b3652d7f2a1a7ab1e
Capaldini <nickcapaldini@gmail.com>
2025-06-04 10:02:36 -0500

    outline for code

---
+++ 05-PresentRec.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+# Streamlit Documentation: https://docs.streamlit.io/
+
+# Title Page
+st.title('BikeTrend App Demo')
+
+
+# ---
+
+# **Authors:**
+# [Salah Mohamoud](mailto:salah.mohamoud.dev@gmail.com),
+# [Sai Keertana Lakku](mailto:saikeertana005@gmail.com),
+# [Zhen Zhuang](mailto:zhuangzhen17cs@gmail.com),
+# [Nick Capaldini](mailto:nick.capaldini@ridethenextwave.com), Ride The Next Wave, May 19, 2025
+
+# ---
+
+
